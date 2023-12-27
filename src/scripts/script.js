7641c9049894b73f188dc5cfb4ff3b22c0897d6c
meriam <meriam.hamdaoui@gmail.com>
2023-12-27 16:55:16 +0100

    fix the navbar background color, add a blur to it

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,30 @@
 /** change the navbar style on scroll action */
 const scrollNavbar = () => {
-  // select html elements
+  // detect the navbar element
   let navbar = document.querySelector("#navbar");
+  // select the reference point, where the navbar will change
   let heroHeader = document.querySelector("#hero-header");
 
-  //   get the different heights
-  const scrollingHeight =
+  // calculate the scrolling height
+  const onScrolling =
     window.scrollY + heroHeader.getBoundingClientRect().height;
   const stickyNav = heroHeader.offsetTop;
 
   // change navbar (bg & text colors) base on the difference between the 2 heights
 
+  /* if (onScrolling < stickyNav) {
+    navbar.style.backgroundColor = "transparent";
+    navbar.style.color = "white";
+    navbar.style.boxShadow = "none";
+  } else {
+      navbar.style.backgroundColor = "#f7fafc"; // Change this to your desired bg-gray-100 color
+      navbar.style.color = "#4a5568"; // Change this to your desired text-gray-600 color
+      navbar.style.boxShadow =
+        "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)";
+  }*/
+
   const navClassNames =
-    scrollingHeight > stickyNav
+    onScrolling > stickyNav
       ? ["transparent-nav", "bg-nav"]
       : ["bg-nav", "transparent-nav"];
 
