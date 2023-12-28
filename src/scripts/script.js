c9afeea16f72f39fe9e71b1516b07c5d45356ddf
meriam <meriam.hamdaoui@gmail.com>
2023-12-28 12:56:51 +0100

    update project style

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,6 +11,12 @@ const scrollNavbar = () => {
   const stickyNav = heroHeader.offsetTop;
 
   // change navbar (bg & text colors) base on the difference between the 2 heights
+  const navClassNames =
+    onScrolling > stickyNav
+      ? ["transparent-nav", "bg-nav"]
+      : ["bg-nav", "transparent-nav"];
+
+  navbar.classList.replace(...navClassNames);
 
   /* if (onScrolling < stickyNav) {
     navbar.style.backgroundColor = "transparent";
@@ -23,11 +29,15 @@ const scrollNavbar = () => {
         "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)";
   }*/
 
-  const navClassNames =
-    onScrolling > stickyNav
-      ? ["transparent-nav", "bg-nav"]
-      : ["bg-nav", "transparent-nav"];
+  // for navbar text color, when it passes the banner height
+  const bannerDimension = document
+    .querySelector("#hero-banner")
+    .getBoundingClientRect();
+  const bannerPassed = bannerDimension.top + bannerDimension.height <= 38; // check if hero banner image is passed
+  const navTextColor = bannerPassed
+    ? ["text-white", "text-gray-700"]
+    : ["text-gray-700", "text-white"];
 
-  navbar.classList.replace(...navClassNames);
+  navbar.classList.replace(...navTextColor);
 };
 window.addEventListener("scroll", scrollNavbar);
